5937c42d95fb870aa78ef84e020450dbf41b1cd3
Ohgyuchan <gyu3637@gmail.com>
2022-03-21 21:21:54 +0900

    auth

---
+++ lib/signup_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final _usernameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          minimum: const EdgeInsets.symmetric(horizontal: 40),
+          child: Stack(children: [
+            // Sign Up Form
+            _signUpForm(),
+
+            // Login Button
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: TextButton(
+                  onPressed: () {},
+                  child: const Text('Already have an account? Login.')),
+            )
+          ])),
+    );
+  }
+
+  Widget _signUpForm() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        // Username TextField
+        TextField(
+          controller: _usernameController,
+          decoration: const InputDecoration(
+              icon: Icon(Icons.person), labelText: 'Username'),
+        ),
+
+        // Email TextField
+        TextField(
+          controller: _emailController,
+          decoration:
+              InputDecoration(icon: const Icon(Icons.mail), labelText: 'Email'),
+        ),
+
+        // Password TextField
+        TextField(
+          controller: _passwordController,
+          decoration: const InputDecoration(
+              icon: Icon(Icons.lock_open), labelText: 'Password'),
+          obscureText: true,
+          keyboardType: TextInputType.visiblePassword,
+        ),
+
+        // Sign Up Button
+        TextButton(
+          onPressed: _signUp,
+          child: const Text('Sign Up'),
+        )
+      ],
+    );
+  }
+
+  void _signUp() {
+    final username = _usernameController.text.trim();
+    final email = _emailController.text.trim();
+    final password = _passwordController.text.trim();
+
+    if (kDebugMode) {
+      print('username: $username');
+      print('email: $email');
+      print('password: $password');
+    }
+  }
+}
